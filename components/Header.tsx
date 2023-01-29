ece984358ad491ed03cdf90b275eb5b95cb8d720
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2023-01-29 23:44:22 +0100

    created the header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,47 @@
 /* eslint-disable jsx-a11y/alt-text */
-import Image from "next/image"
+import Image from "next/image";
+import {
+  MagnifyingGlassIcon,
+  Bars4Icon,
+  UserCircleIcon,
+  UsersIcon,
+  GlobeAltIcon,
+} from "@heroicons/react/24/outline";
 
 function Header() {
   return (
- <div className="sticky top-0 z-50 grid-3 grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10">
-    <div className="relative flex items-center h-10 cursor-pointer ">
-        <Image 
-                  src="https://links.papareact.com/qd3"
-                  width={100}
-                  height={100}
-                  style={{ objectFit: "contain", objectPosition: "left" }} alt={""}
-        
+    <header className="sticky top-0 z-50 grid-3 grid-cols-3 bg-white shadow-md p-5 md:px-10 flex flex-row">
+      <div className="relative flex items-center h-10 cursor-pointer my-auto w-1/3">
+        <Image
+          src="https://links.papareact.com/qd3"
+          width={100}
+          height={100}
+          style={{ objectFit: "contain", objectPosition: "left" }}
+          alt={""}
         />
-    </div>
+      </div>
+      <div className="flex flex-row items-center md:border-2  p-2 rounded-full w-2/3  md:shadow-sm">
+        <input
+          type="text"
+          className="pl-5 text-gray-600 placeholder-gray-400 flex-grow bg-transparent outline-none "
+          placeholder="Start your search"
+        />
+        <MagnifyingGlassIcon className="hidden md:inline-flex h-8 text-white bg-red-500 cursor-pointer rounded-full p-2 md:mx-2" />
+      </div>
+      <div
+        className="flex space-x-4 items-center text-gray-500 justify-end text-gray"
+        style={{ width: "100%" }}
+      >
+        <p>Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
 
-    {/* search section */}
-
-    <div>
-        <input type='text' placeholder='Start your search' />
-    </div>
-    </div>
- 
-  )
+        <div className="flex space-x-2 items-center border-2 p-2 cursor-pointer rounded-full">
+            <Bars4Icon className="h-6"/>
+            <UserCircleIcon className="h-6"/>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
